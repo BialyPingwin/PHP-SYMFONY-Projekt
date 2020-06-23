2ebd789a144eeaf6470b19d05a547c867daf8332
BialyPingwin <48740709+BialyPingwin@users.noreply.github.com>
2020-06-23 17:19:48 +0200

    twig updates

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Comments;
 use App\Entity\Post;
 use DateTime;
@@ -16,10 +17,32 @@ class BlogController extends AbstractController
             'controller_name' => 'BlogController',
         ]);
     }
-    public function show_post($post_id)
+    public function show_post($post_id,Request $request)
     {
+
+
+
+        $comment = new Comments();
+        
+        //$post->setPostId($post->getId());
+        //$post->setDate(new \DateTimeInterface('now'));
+        // $form = $this->createForm(Comments::class, $comment);
+
+        // $form->handleRequest($request);
+        // if ($form->isSubmitted()){
+        //     $comment = $form->getData();            
+        //     $comment->setDate(new \DateTime('now'));
+        //     $entityManager = $this->getDoctrine()->getManager();
+        //     $entityManager->persist($comment);
+        //     $entityManager->flush();
+        //     return $this->redirectToRoute('post', $post_id);
+        // }
+
+
         return $this->render('blog/post.html.twig', [
             'controller_name' => 'PostController '.$post_id ,
+            'form' => 'form',
+            'post_comments' => 'null',
         ]);
     }
 }
--- src/Form/CommentsType.php
+++ src/Form/CommentsType.php
@@ -6,16 +6,18 @@ use App\Entity\Comments;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CommentsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('author')
-            ->add('text')
-            ->add('date')
-            ->add('post_id')
+            ->add('author', TextType::class)
+            ->add('text', TextareaType::class)
+            ->add('submit', SubmitType::class, ['label' => 'Submit'])             
         ;
     }
 
