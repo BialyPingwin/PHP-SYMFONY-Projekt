f2632c26c2c50bdf7f665c5675ccee1d6cb35608
iMarcin <64076685+iMarcin@users.noreply.github.com>
2020-06-23 16:58:37 +0200

    pokazposta

---
+++ src/Controller/PokazpostController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Comments;
+use App\Entity\Post;
+use DateTime;
+use Symfony\Component\HttpFoundation\Response;
+
+class PokazpostController extends AbstractController
+{
+    /**
+     * @Route("/pokazpost", name="pokazpost")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/PokazpostController.php',
+        ]);
+    }
+    public function  id($id)
+    {
+        $id=$this->getDoctrine()
+            ->getRepository(Post::class)
+            ->find($id);
+        if(!$id){
+            return new Response('nie ma ID');
+        }
+        return new Response('Author:'.$id->getAuthor().' Title:'.$id->getTitle().' Text:'.$id->getText());
+    }
+}
